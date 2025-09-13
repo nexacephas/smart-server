42897e392affb2157d3b90320760980566c20731
cephas <preciousayo773@gmail.com>
2025-09-13 14:58:56 +0100

    Update Firebase admin configuration and server setup

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,12 +1,20 @@
-// server/firebase/admin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // download from Firebase console
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://smart-meter-2025-c7b32-default-rtdb.firebaseio.com", // your Realtime DB URL
+  credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.database();
-
-module.exports = db;
+module.exports = admin;
